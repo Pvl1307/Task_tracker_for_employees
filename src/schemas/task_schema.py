27464b00101d368fb57e16a889740b34ea204c31
Pvl1307 <ivanov.pavel.1307@gmail.com>
2023-12-01 14:41:01 +0300

    Fixed tasks' schemas

--- src/schemas/task_schema.py
+++ src/schemas/task_schema.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class TaskBase(BaseModel):
     title: str
     description: str
+    deadline: datetime
+    status: str
 
 
 class TaskCreate(TaskBase):
