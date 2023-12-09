028cce2d51d8eacc84d74c5148e269f86c98f023
Pvl1307 <ivanov.pavel.1307@gmail.com>
2023-12-09 23:33:39 +0300

    CRUD classes & new migration(final migration)

--- src/crud/employee_crud.py
+++ src/crud/employee_crud.py
@@ -1,100 +1,65 @@
-from sqlalchemy.orm import Session
+from sqlalchemy import func
+from sqlalchemy.orm import Session, joinedload
 
-from src.models import Employee
+from src.models import Employee, Task
 from src.schemas.employee_schema import EmployeeCreate, EmployeeUpdate
 
 
-def get_employee(db: Session, employee_id: int):
-    """Вывод сотрудника по id (GET)"""
-    return db.query(Employee).filter_by(id=employee_id).first()
+class EmployeeCRUD:
+    def __init__(self, db: Session):
+        self.db = db
 
+    def get_employee(self, employee_id: int):
+        """Вывод сотрудника по id (GET)"""
+        return self.db.query(Employee).filter_by(id=employee_id).first()
 
-def get_all_employees(db: Session, skip: int = 0, limit: int = 100):
-    """Вывод всех сотрудников с возможностью пагинации (GET)
-    skip - количество, которое нужно в начале пропустить
-    limit - лимит сотрудников на вывод"""
-    return db.query(Employee).offset(skip).limit(limit).all()
+    def get_all_employees(self, skip: int = 0, limit: int = 100):
+        """Вывод всех сотрудников с возможностью пагинации (GET)
+        skip - количество, которое нужно в начале пропустить
+        limit - лимит сотрудников на вывод"""
+        return self.db.query(Employee).offset(skip).limit(limit).all()
 
+    def get_busy_employees(self):
+        """Запрос сотрудников и их задач, отсортированных по количеству активных задач."""
+        query = (
+            self.db.query(Employee)
+            .outerjoin(Task, Employee.id == Task.executor_id)
+            .group_by(Employee.id)
+            .order_by(func.count(Task.id).desc())
+            .options(joinedload(Employee.tasks))
+            .all())
 
-def create_employee(db: Session, employee: EmployeeCreate):
-    """Создание сотрудника (CREATE)"""
-    is_busy_value = employee.is_busy if employee.is_busy is not None else False
+        return query
 
-    db_employee = Employee(fullname=employee.fullname,
-                           position=employee.position,
-                           is_busy=is_busy_value)
-    db.add(db_employee)
-    db.commit()
-    db.refresh(db_employee)
-    return db_employee
+    def create_employee(self, employee_schema: EmployeeCreate):
+        """Создание сотрудника (CREATE)"""
+        is_busy_value = employee_schema.is_busy if employee_schema.is_busy is not None else False
 
+        db_employee = Employee(fullname=employee_schema.fullname,
+                               position=employee_schema.position,
+                               is_busy=is_busy_value)
+        self.db.add(db_employee)
+        self.db.commit()
+        self.db.refresh(db_employee)
+        return db_employee
 
-def update_employee(db: Session, employee_id: int, employee_data: EmployeeUpdate):
-    """Обновление сотрудника (PUT, PATCH)"""
-    db_employee = db.query(Employee).filter_by(id=employee_id).first()
+    def patch_employee(self, employee_schema: EmployeeUpdate, employee_id: int):
+        """Обновление сотрудника (PATCH)"""
+        db_employee = self.db.query(Employee).filter_by(id=employee_id).first()
 
-    if db_employee:
-        for field, value in employee_data.model_dump(exclude_unset=True).items():
-            setattr(db_employee, field, value)
-        db.commit()
-        db.refresh(db_employee)
+        if db_employee:
+            for field, value in employee_schema.model_dump(exclude_unset=True).items():
+                setattr(db_employee, field, value)
+            self.db.commit()
+            self.db.refresh(db_employee)
 
-    return db_employee
+        return db_employee
 
+    def delete_employee(self, employee_id: int):
+        """Удаление сотрудника (DELETE)"""
+        db_employee = self.db.query(Employee).filter_by(id=employee_id).first()
+        if db_employee:
+            self.db.delete(db_employee)
+            self.db.commit()
 
-def delete_employee(db: Session, employee_id: int):
-    """Удаление сотрудника (DELETE)"""
-    db_employee = db.query(Employee).filter_by(id=employee_id).first()
-    if db_employee:
-        db.delete(db_employee)
-        db.commit()
-
-    return db_employee
-
-# def get_busy_employee(db: Session):  # Пока не трогал в урлах!!!!!!!
-#     """Вывод списка сотрудников и их задачи, отсортированный по количеству активных задач (GET)"""
-#     return (db.query(Employee, func.count(Task.id).label('task_count'))
-#             .join(Task, Employee.id == Task.executor_id)
-#             .filter_by(Employee.is_busy)
-#             .group_by(Employee.id)
-#             .order_by(func.count(Task.id).desc())
-#             .all()
-#             )
-#
-#
-# def get_available_employees(db: Session):  # Пока не трогал в урлах!!!!!!!
-#     """Вывод сотрудникам, которые могут взять такие задачи
-#     (наименее загруженный сотрудник или сотрудник выполняющий родительскую задачу,
-#     если ему назначено максимум на 2 задачи больше,
-#     чем у наименее загруженного сотрудника)"""
-#     employees_with_tasks_count = (
-#         db.query(Employee, func.count(Task.id).label('task_count')
-#                  .join(Task, Employee.id == Task.executor_id)
-#                  .filter(Employee.is_busy is True)
-#                  .groupby(Employee.id)
-#                  .all()
-#                  )
-#     )
-#
-#     sorted_employees = sorted(employees_with_tasks_count, key=lambda x: x.task_count)
-#
-#     available_employees = sorted_employees[:2]
-#
-#     important_tasks = []
-#     for employee, task_count in available_employees:
-#         tasks = (
-#             db.query(Task)
-#             .filter(Task.executor_id == employee.id)
-#             .filter(Task.status != 'Completed')  # Выбрать невыполненные задачи
-#             .order_by(Task.deadline)
-#             .all()
-#         )
-#
-#         for task in tasks:
-#             important_tasks.append({
-#                 'Важная задача': task.title,
-#                 'Срок': task.deadline,
-#                 'ФИО сотрудника': employee.fullname
-#             })
-#
-#     return important_tasks
+        return db_employee
--- src/crud/task_crud.py
+++ src/crud/task_crud.py
@@ -1,67 +1,139 @@
-from sqlalchemy.orm import Session
+from datetime import datetime
 
-from src.models import Task
-from src.schemas.task_schema import TaskCreate, TaskUpdate
+from sqlalchemy import and_, func, or_
+from sqlalchemy.orm import Session, joinedload
 
-
-def get_task(db: Session, task_id: int):
-    """Вывод задания по id (GET)"""
-    return db.query(Task).filter_by(id=task_id).first()
-
-
-def get_all_tasks(db: Session, skip: int = 0, limit: int = 100):
-    """Вывод всех заданий с возможностью пагинации (GET)
-    skip - количество, которое нужно в начале пропустить
-    limit - лимит заданий на вывод"""
-    return db.query(Task).offset(skip).limit(limit).all()
+from src.models import Task, Employee
+from src.models.task_model import TaskStatus
+from src.schemas.task_schema import TaskCreate, TaskUpdate, ImportantTask
 
 
-def get_important_tasks(db: Session):
-    """Возврашает задачи, не взятые в работу,
-    и от которых зависят другие задачи, взятые в работу (GET)"""
-    return (db.query(Task)
-            .join(Task, Task.id == Task.parent_task_id, isouter=True)
-            .filter(Task.status != 'Started')
-            .filter(Task.parent_task_id is not None)
-            .filter(Task.parent_task.has(status='Started'))
-            .all()
+class TaskCRUD:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_task(self, task_id: int):
+        """Вывод задания по id (GET)"""
+        return self.db.query(Task).filter_by(id=task_id).first()
+
+    def get_all_tasks(self, skip: int = 0, limit: int = 100):
+        """Вывод всех заданий с возможностью пагинации (GET)
+        skip - количество, которое нужно в начале пропустить
+        limit - лимит заданий на вывод"""
+        return self.db.query(Task).offset(skip).limit(limit).all()
+
+    def create_task(self, task_schema: TaskCreate):
+        """Создание задания (CREATE)"""
+        db_task = Task(parent_task_id=task_schema.parent_task_id,
+                       title=task_schema.title,
+                       description=task_schema.description,
+                       deadline=task_schema.deadline,
+                       status=task_schema.status,
+                       executor_id=task_schema.executor_id,
+                       created_at=datetime.utcnow()
+                       )
+        self.db.add(db_task)
+        self.db.commit()
+        self.db.refresh(db_task)
+
+        return db_task
+
+    def patch_task(self, task_id: int, task_schema: TaskUpdate):
+        """Обновление задания (PATCH)"""
+        db_task = self.db.query(Task).filter_by(id=task_id).first()
+
+        if db_task:
+            for field, value in task_schema.model_dump(exclude_unset=True).items():
+                setattr(db_task, field, value)
+            self.db.commit()
+            self.db.refresh(db_task)
+
+        return db_task
+
+    def delete_task(self, task_id: int):
+        """Удаление задания (DELETE)"""
+        db_task = self.db.query(Task).filter_by(id=task_id).first()
+        if db_task:
+            self.db.delete(db_task)
+            self.db.commit()
+
+        return db_task
+
+    def get_important_tasks(self):
+        """Вывод задач, не взятых в работу, и сотрудников, способных взять их (GET)"""
+        important_tasks = (self.db.query(Task)
+                           .filter(and_(Task.status == TaskStatus.NOT_STARTED,
+                                        Task.subtasks.any(Task.status == TaskStatus.STARTED)))
+                           .options(joinedload(Task.executor))
+                           .all()
+                           )
+
+        important_tasks_list = []
+
+        for task in important_tasks:
+            suitable_employee = (self.db.query(Employee)
+                                 .outerjoin(Task, Employee.id == Task.executor_id)
+                                 .group_by(Employee.id)
+                                 .having(or_(Employee.id == task.executor_id, func.count(Task.id) <= 2))
+                                 .order_by(func.count(Task.id))
+                                 .first()
+                                 )
+
+            important_task = ImportantTask(
+                title=task.title,
+                deadline=task.deadline,
+                executor_name=suitable_employee.fullname if suitable_employee else None
             )
+            important_tasks_list.append(important_task)
+
+        return important_tasks_list
 
 
-def create_task(db: Session, task: TaskCreate):
-    """Создание задания (CREATE)"""
-    db_task = Task(parent_task_id=task.parent_task_id,
-                   title=task.title,
-                   description=task.description,
-                   deadline=task.deadline,
-                   status=task.status,
-                   executor_id=task.executor_id
-                   )
-    db.add(db_task)
-    db.commit()
-    db.refresh(db_task)
-
-    return db_task
 
 
-def update_task(db: Session, task_id: int, task_data: TaskUpdate):
-    """Обновление задания (PUT, PATCH)"""
-    db_task = db.query(Task).filter_by(id=task_id).first()
-
-    if db_task:
-        for field, value in task_data.model_dump(exclude_unset=True).items():
-            setattr(db_task, field, value)
-        db.commit()
-        db.refresh(db_task)
-
-    return db_task
 
 
-def delete_task(db: Session, task_id: int):
-    """Удаление задания (DELETE)"""
-    db_task = db.query(Task).filter_by(id=task_id).first()
-    if db_task:
-        db.delete(db_task)
-        db.commit()
 
-    return db_task
+        # important_tasks = (
+        #     self.db.query(Task)
+        #     .filter(
+        #         and_(
+        #             Task.status == TaskStatus.NOT_STARTED,
+        #             Task.parent_task.has(Task.status == TaskStatus.STARTED))
+        #     )
+        #     .options(joinedload(Task.executor))
+        #     .all()
+        # )
+        #
+        # results = []
+        # for important_task in important_tasks:
+        #     if important_task not in results:
+        #         executors = []
+        #
+        #         if important_task.executor_id:
+        #
+        #             parent_task_executor = important_task.parent_task.executor
+        #             if parent_task_executor and (
+        #                     parent_task_executor.tasks.count() <= 2
+        #             ):
+        #                 executors.append(parent_task_executor.fullname)
+        #
+        #             if not executors:
+        #                 least_busy_executor = (
+        #                     self.db.query(Employee)
+        #                     .select_from(Employee)
+        #                     .outerjoin(Task, Employee.id == Task.executor_id)
+        #                     .group_by(Employee.id)
+        #                     .having(
+        #                         min(func.count(Task.id)) <= 2
+        #                     )
+        #                     .subquery()
+        #                 )
+        #
+        #                 if least_busy_executor:
+        #                     executors.append(least_busy_executor.first().fullname)
+        #
+        #         important_task.executor_names = executors
+        #         results.append(important_task)
+        #
+        # return results
